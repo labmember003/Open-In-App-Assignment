1610841511da26e510bfecfe0e8783b16ff44da5
Avi <avi@Anirudhs-MacBook-Pro-2.local>
2024-01-27 13:39:10 +0530

    added data classes

---
+++ app/src/main/java/DataX.kt
@@ -0,0 +1,6 @@
+data class DataX(
+    val favourite_links: List<Any>,
+    val overall_url_chart: OverallUrlChart,
+    val recent_links: List<RecentLink>,
+    val top_links: List<TopLink>
+)
---
+++ app/src/main/java/RawData.kt
@@ -0,0 +1,16 @@
+data class RawData(
+    val applied_campaign: Int,
+    val `data`: DataX,
+    val extra_income: Double,
+    val links_created_today: Int,
+    val message: String,
+    val startTime: String,
+    val status: Boolean,
+    val statusCode: Int,
+    val support_whatsapp_number: String,
+    val today_clicks: Int,
+    val top_location: String,
+    val top_source: String,
+    val total_clicks: Int,
+    val total_links: Int
+)
